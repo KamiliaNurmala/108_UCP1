ea09d99de26331552059a3d12830c0209d8ac9c5
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-05 10:12:43 +0700

    Change DATE be DATETIME

--- models/hotel.js
+++ models/hotel.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull : false
         },
         Tanggal_Pesan : {
-            type : DataTypes.DATE,
+            type : DataTypes.DATETIME,
             allowNull : true
         }
     }, {
